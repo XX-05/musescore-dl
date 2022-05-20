b1218096c282a543ee6e9fa745a15d1319bbe596
XX-05 <matt.r.aharonian@gmail.com>
2022-05-20 00:25:54 -0400

    Update setup.py requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 setup(
     name="musescore-dl",
     version="0.1.0",
-    py_modules=["main"],
-    install_requires=["beautifulsoup4", "lxml", "requests", "svglib"],
+    install_requires=["beautifulsoup4", "lxml", "requests", "svglib", "questionary"],
     entry_points={
         "console_scripts": [
             "musescore-dl = musescoredl:cli",
